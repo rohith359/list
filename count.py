257a235e05096040bf9e56b505a7afa7d4fe57ff
rohith359 <srohith2584@gmail.com>
2024-09-19 14:38:30 +0530

    count.py

---
+++ count.py
@@ -0,0 +1,18 @@
+***
+Write a program to count the number of times the given value is repeated.
+Input Format:
+First line of input consists of our list elements.
+Second line of input consists of value to count.
+Output Format:
+Print thr number of times the given value is repeated in the list.
+Sample Input:
+10 20 10 40 10
+10
+Sample Output:
+3
+***
+elements = list(map(int, input().strip().split()))
+value_to_count = int(input().strip())
+
+count = elements.count(value_to_count)
+print(count)
